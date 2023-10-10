f16adfdb5ec7f7f6b10ea84598ea7e54727bae5a
unknown <muddabir22@gmail.com>
2023-10-10 23:31:57 +0500

    object operations javascript (1)

---
+++ object.js
@@ -0,0 +1,9 @@
+const data = {
+    name: "chohan",
+    age: 26,
+    gender: "male",
+    designation: "consultant"
+}
+
+//the same functionality with built in constructor method Object.keys
+console.log(Object.keys(data))
