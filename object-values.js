10046e5df1850cf76c9f79be4da94f7a9b5397aa
unknown <muddabir22@gmail.com>
2023-10-19 23:00:23 +0500

    fetch object values

---
+++ object-values.js
@@ -0,0 +1,34 @@
+const data = {
+    name: "chohan",
+    age: 26,
+    gender: "male",
+    designation: "consultant"
+}
+
+
+
+// one liner solution with built in method
+console.log(Object.values(data))
+
+
+
+
+
+
+// **   Object VALUES  **
+// lets try with a for in loop
+// let list =[]
+// for(const item in data){
+//     list.push(data[item])
+// }
+
+// //console the final array
+// console.log("item",list)
+
+
+
+
+
+
+
+
