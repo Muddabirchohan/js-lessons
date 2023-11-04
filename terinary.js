48149c9cdc5605fe71b567b05f9e69f4b3232385
unknown <muddabir22@gmail.com>
2023-11-04 16:32:26 +0500

    multiple terinary operators acting as if else

---
+++ terinary.js
@@ -0,0 +1,26 @@
+const renderRole = (role) => {
+
+    if (role === "admin") {
+        console.log("admin user")
+    } else if (role === "subAdmin") {
+        console.log("sub admin")
+    } else {
+        console.log("default")
+    }
+
+}
+
+// role === "admin" ? console.log("admin user") : role === "subAdmin" ? console.log("sub admin") : console.log("default")
+
+
+role === "admin" ? 
+
+console.log("inside if block") 
+
+: role === "subAdmin" ? 
+
+console.log("inside else if") 
+
+: console.log("else")
+
+
